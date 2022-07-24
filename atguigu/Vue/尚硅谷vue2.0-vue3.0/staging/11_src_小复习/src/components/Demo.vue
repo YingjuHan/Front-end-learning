89efa59a55c325c928ed2ce76f8a8e46136ffee8
YingjuHan <hanyingju@live.cn>
2022-07-24 23:29:39 +0800

    all

---
+++ atguigu/Vue/尚硅谷vue2.0-vue3.0/staging/11_src_小复习/src/components/Demo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="demo">
+    <h3>我是Demo组件</h3>
+    <h4>我有一台电脑，是{{ computer }}</h4>
+    <button @click="sedComp">把电脑传给父亲</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Demo',
+  data() {
+    return {
+      computer: '联想拯救者'
+    };
+  },
+  methods: {
+    sedComp() {
+      this.$emit('ha-ha', this.computer);
+    }
+  },
+  mounted() {
+    this.$bus.$on('shui-guo', function (f, a, b, c) {
+      console.log('Demo收到了', f, a, b, c);
+    });
+  },
+  // 11_src_小复习
+};
+</script>
+
+<style>
+.demo {
+  background-color: orange;
+  padding: 5px;
+}
+</style>
