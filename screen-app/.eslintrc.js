4b332d7e14eb9dd087e77159e0c991853700ce62
YingjuHan <hanyingju@live.cn>
2023-07-11 22:38:35 +0800

    set eslint setting

--- screen-app/.eslintrc.js
+++ screen-app/.eslintrc.js
@@ -15,5 +15,14 @@ module.exports = {
     rules: {
         "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "prettier/prettier": "off",
+        "vue/no-unused-components": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "no-irregular-whitespace": "off", //禁止掉空格报错检查
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-empty-function": "off",
+        "@typescript-eslint/no-inferrable-types": "off", // 关闭类型推断
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/ban-ts-comment": "off",
     },
 };
