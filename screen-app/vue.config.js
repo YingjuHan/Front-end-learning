920e177b3e161b2792fb6d44de25e002101c6ada
YingjuHan <hanyingju@live.cn>
2023-07-11 22:54:50 +0800

    Webpack5 syntax modification

--- screen-app/vue.config.js
+++ screen-app/vue.config.js
@@ -18,16 +18,17 @@ module.exports = {
   },
   //scss全局变量 change-mark
   css: {
-    // 启用 CSS modules
-    requireModuleExtension: false,
-    // 是否使用css分离插件
-    extract: true,
-    // 开启 CSS source maps，一般不建议开启
-    sourceMap: false,
+    // css预设器配置项
     loaderOptions: {
-      scss: {
-        prependData: `@import "@/scss/variables.scss";`,
+      sass:{
+        additionalData: `@import "@/scss/variables.scss";`,
       },
+      css:{
+        module:{
+          auto:()=>true
+        }
+      }
     },
+    // requireModuleExtension: true
   },
 };
