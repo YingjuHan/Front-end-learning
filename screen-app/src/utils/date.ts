5ad087a0535e7c19a96a1f2e196085f7ad5291a2
YingjuHan <hanyingju@live.cn>
2023-07-15 20:06:27 +0800

    add date

---
+++ screen-app/src/utils/date.ts
@@ -0,0 +1,30 @@
+import moment from "moment";
+import "moment/local/zh-cn";
+
+moment.locale("zh-cn");
+moment.suppressDeprecationWarnings = true; // 禁用弃用警告
+
+const weeks = [
+    "星期日",
+    "星期一",
+    "星期二",
+    "星期三",
+    "星期四",
+    "星期五",
+    "星期六",
+];
+
+function getWeekByData(date: Date): string {
+    const weekIndex = date.getDay();
+    return weeks[weekIndex];
+}
+
+export function getCurDataTime(): string {
+    return (
+        moment().format("YYYY-MM-DD") +
+        " " +
+        getWeekByData(new Date()) +
+        " " +
+        moment().format("HH:mm:ss")
+    );
+}
